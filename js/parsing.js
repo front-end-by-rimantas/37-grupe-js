02f194458c424ec129f807cc4c3f5aa998832310
Rimantas <belauzas@yahoo.com>
2022-06-16 11:17:03 +0300

    Type casting into number with plus operator

---
+++ js/parsing.js
@@ -0,0 +1,25 @@
+console.clear();
+
+console.log('----- parseInt');
+console.log(parseInt(5));
+console.log(parseInt(5.3));
+console.log(parseInt('5'));
+console.log(parseInt('5a'));
+console.log(parseInt('a'));
+console.log(parseInt('a5'));
+
+console.log('----- parseFloat');
+console.log(parseFloat(5));
+console.log(parseFloat(5.3));
+console.log(parseFloat('5'));
+console.log(parseFloat('5a'));
+console.log(parseFloat('a'));
+console.log(parseFloat('a5'));
+
+console.log('----- +');
+console.log(+5);
+console.log(+5.3);
+console.log(+'5');
+console.log(+'5a');
+console.log(+'a');
+console.log(+'a5');
