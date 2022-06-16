b9a898bf37c8a26cc01262db1c52c059fd348169
Rimantas <belauzas@yahoo.com>
2022-06-16 11:49:04 +0300

    Truphy falsy values

---
+++ js/if-values.js
@@ -0,0 +1,28 @@
+console.clear();
+
+/*
+REIKSMIU POZITYVUMAS / NEGATYVUMAS
+skaiciai:
+    0 - negatyvus
+    kiti - pizityvus
+boolean:
+    true - pozityvus
+    false - negatyvus
+string:
+    ne tuscias - pozityvus
+    tuscias - negatyvus
+array:
+    visada pozityvus
+object:
+    visada pozityvus
+function:
+    visada pozityvus
+*/
+
+const userInput = ' ';
+
+if (userInput) {
+    console.log('taip');
+} else {
+    console.log('ne');
+}
